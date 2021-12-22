e77c2900f8083d55244bb8d10ff93c4865dbbdd6
wael-ali <waelali595@gmail.com>
2021-12-22 21:58:25 +0100

    create initial db migration

---
+++ migrations/Version20211222205614.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211222205614 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, date DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE participation (id INT AUTO_INCREMENT NOT NULL, attendant_id INT NOT NULL, event_id INT NOT NULL, fee INT NOT NULL, INDEX IDX_AB55E24F4DE0C235 (attendant_id), INDEX IDX_AB55E24F71F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE participation ADD CONSTRAINT FK_AB55E24F4DE0C235 FOREIGN KEY (attendant_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE participation ADD CONSTRAINT FK_AB55E24F71F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participation DROP FOREIGN KEY FK_AB55E24F71F7E88B');
+        $this->addSql('ALTER TABLE participation DROP FOREIGN KEY FK_AB55E24F4DE0C235');
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE participation');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
